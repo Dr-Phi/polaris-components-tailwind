11a92e1371644580a3834c81111eb99460d51d92
Dr-Phi <eliasrojaslon@gmail.com>
2023-04-05 22:33:35 -0500

    Landing page & first component added, all set up

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./components.html", "./index.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
